218018649067f95a440830235fb82fffcb2a1f32
Kristen Prescott <prescottdarling@gmail.com>
2021-06-28 21:38:19 -0400

    styling changes

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.header}>
+        <Text style={styles.headerText}>GameZone</Text>
+      </View>
+      <View style={styles.body}>
+        <Text style={styles.bodyText}>Lorem ipsum yadda yadda bleh...</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +19,25 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#F3F4F7",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  header: {
+    backgroundColor: "#AA5042",
+    padding: 20,
+  },
+  headerText: {
+    fontSize: 42,
+    fontWeight: "900",
+    color: "#253031",
+  },
+  body: {
+    backgroundColor: "#508991",
+    padding: 20,
+  },
+  bodyText: {
+    color: "#253031",
+    fontSize: 18,
   },
 });
